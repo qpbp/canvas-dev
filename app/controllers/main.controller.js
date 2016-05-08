2b8b5cbff257bbd03fdb2dd7320f849b2f0dceae
ARoman <qpbd@ukr.net>
2016-05-08 18:12:55 +0300

    vertical stretch, hiding the bounding/modification box before generating the image, constraining the logo to the target area

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -10,12 +10,15 @@ angular.module('app')
       logoUrl: null
     };
 
+    //base64 data empty variable for generated image
     $scope.generatedImage = "";
 
+    //the product data, we need x,y for positioning
     $scope.product = {
       url: "assets/images/product.png",
       top: 165,
-      left: 180
+      left: 180,
+      canvas: {height: 200, width: 100}
     };
 
     //the list of logos
@@ -27,23 +30,28 @@ angular.module('app')
 
     //run canvas with and give it size
     $scope.canvas = new fabric.Canvas('editor');
-    $scope.canvas.setHeight(200);
-    $scope.canvas.setWidth(100);
+    $scope.canvas.setHeight($scope.product.canvas.height);
+    $scope.canvas.setWidth($scope.product.canvas.width);
 
     //selectLog function
     //param: logo image link
     $scope.selectLogo = function (logo) {
       var logo = logo || "";
+      //clearing canvas
       $scope.canvas.clear();
       fabric.Image.fromURL(logo, function (img) {
 
+
+        //$scope.product.canvas.height && $scope.product.canvas.width
+        //for centering object
         var obj = {
           scaleX: $scope.canvas.width / img.width,
+          scaleY: $scope.canvas.height / (2 * img.height),
           top: 2,
           left: 2
         };
 
-        if (lastAdded) {
+        if (lastAdded.top && lastAdded.left) {
           obj.top = lastAdded.top;
           obj.left = lastAdded.left;
         }
@@ -56,26 +64,29 @@ angular.module('app')
 
       $scope.canvas.renderAll();
     };
+
     //generate function
     $scope.generateImage = function () {
+
       //upload product image
       fabric.Image.fromURL($scope.product.url, function (picture) {
-
+        //the next line removing selection around the object on canvas
+        $scope.canvas.deactivateAll().renderAll()
+        //upload canvas print in base64 data
         fabric.Image.fromURL($scope.canvas.toDataURL(), function (canvas) {
 
+          $scope.canvas.getActiveObject();
+          //set coordinates by shift (see $scope.product variable)
           var img = canvas.set({
-            left: 180,
-            top: 165
+            left: $scope.product.left,
+            top: $scope.product.top
           });
 
-          img.selectable = false;
-          
+          //create group
           var group = new fabric.Group([picture, img]);
 
           $scope.generatedImage = group.toDataURL();
-
           $scope.$apply();
-
         });
       });
     };
@@ -88,10 +99,5 @@ angular.module('app')
       lastAdded.logoUrl = object.logoUrl;
       $scope.$apply()
     });
-
-    /*
-
-    //  $scope.canvas.renderAll();
-    //  */
 
   }]);
