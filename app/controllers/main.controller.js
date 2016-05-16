aca1c50c224ba3f82132d8bac2ecfaa4a5d5ebc2
ARoman <qpbd@ukr.net>
2016-05-16 16:56:10 +0300

    added comments

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -14,7 +14,8 @@ angular.module('app')
     //base64 data empty variable for generated image
     $scope.generatedImage = "";
 
-    //the product data, we need x,y for positioning
+    //the product data. We need top,left for positioning canvas area.
+    //canvas height, width - for canvas
     $scope.product = {
       url: "assets/images/product.png",
       top: 165,
@@ -22,18 +23,27 @@ angular.module('app')
       canvas: {height: 200, width: 100}
     };
 
-    //the list of logos
+    //the list of logos links
     $scope.logos = ['logo1.png', 'logo2.png', 'logo3.png', 'logo4.png', 'logo5.png'].map(function (imageUrl) {
       //this comfortable if we will change the basic URL
       var basicUrl = "assets/images/";
       return basicUrl + imageUrl;
     });
 
-    //run canvas with and give it size
+    //init canvas and give it size
     $scope.canvas = new fabric.Canvas('editor');
     $scope.canvas.setHeight($scope.product.canvas.height);
     $scope.canvas.setWidth($scope.product.canvas.width);
 
+    //here we dynamically set the shift for image
+    //so its will be easily to change if we will have another image
+    var elem = angular.element(document.querySelector(".canvas-container"));
+    elem.ready(function(){
+      elem.css('top', $scope.product.top + "px");
+      elem.css('left', $scope.product.left + "px");
+    });
+
+    //make transparenr corners and move rotating point closer to rectangle
     fabric.Object.prototype.set({
       transparentCorners: true,
       rotatingPointOffset: 0
@@ -42,12 +52,13 @@ angular.module('app')
     //removing the top scale
     fabric.Object.prototype.setControlsVisibility({mt: false});
 
-    //selectLog function
+    //selectLogo function
     //param: logo image link
     $scope.selectLogo = function (logo) {
       var logo = logo || "";
       //clearing canvas
       $scope.canvas.clear();
+      //load image
       fabric.Image.fromURL(logo, function (img) {
 
         //$scope.product.canvas.height && $scope.product.canvas.width
@@ -55,11 +66,11 @@ angular.module('app')
 
         // cw, ch - canvas dimestions
         // iw, ih - image dimestions
+        //the ratio
         var fw, fh;
         var width_ratio = $scope.canvas.width / img.width;
         var height_ratio = $scope.canvas.height / img.height;
 
-        //this
         fw = img.width * width_ratio;
         fh = img.height * fw / img.width;
 
@@ -78,22 +89,28 @@ angular.module('app')
          fw = iw*fh/ih;
          }
          */
+        //set height,width
         var obj = {
           width: fw,
           height: fh
         };
 
+        //set last logo coordinates
         if (lastAdded.top && lastAdded.left) {
           obj.top = lastAdded.top;
           obj.left = lastAdded.left;
         }
 
+        //set properties to logo
         img.set(obj);
 
+        //set url to object
         img.logoUrl = logo;
-
+        //center logo image
         $scope.canvas.centerObject(img);
+        //add on canvas
         $scope.canvas.add(img);
+        //render canvas
         $scope.canvas.renderAll();
       });
 
@@ -118,13 +135,17 @@ angular.module('app')
 
           //create group
           var group = new fabric.Group([picture, img]);
-
+          //create base64
           $scope.generatedImage = group.toDataURL();
+          //update changes
           $scope.$apply();
         });
       });
     };
 
+    //here we make strict all sides in canvas
+    //using in moving and scaling watchers
+    //the logic taken from stackoverflow.com
     function keepPosFixed(obj) {
 
       obj.setCoords();
@@ -140,12 +161,14 @@ angular.module('app')
       }
     };
 
+    //watch object while moving
     $scope.canvas.on('object:moving', function (e) {
       var obj = e.target;
       // if object is too big ignore
       if (obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width) {
         return;
       }
+      // we use function below, to keep object in canvas area
       keepPosFixed(obj);
     });
 
@@ -156,16 +179,23 @@ angular.module('app')
         actualWidth = shape.scaleX * shape.width,
         actualHeight = shape.scaleY * shape.height;
 
+      //the rotation from 0 to 360 of object
       var angle = shape.getAngle() % 360;
       var floorAngle = Math.floor(angle);
 
+
+      //here we check positiong of object (its vertical or horisontal).
+      //if horisontal we cant scale over maxWidth length
+      //if vertical we cant scale over maxHeight length
       if (!( (315 < floorAngle && floorAngle <= 359)
           || (0 < floorAngle && floorAngle <= 45)
           || (135 < floorAngle && floorAngle <= 225)
         )) {
+        //so we swap maxVariables while scaling, based on angle
         actualHeight = [actualWidth, actualWidth = actualHeight][0];
       }
 
+      //max scale
       if (actualHeight >= maxHeight || actualWidth >= maxWidth) {
         var scalef = (maxHeight / shape.height) < (maxWidth / shape.width) ? (maxHeight / shape.height) : (maxWidth / shape.width);
         shape.set({
@@ -173,6 +203,7 @@ angular.module('app')
           scaleX: scalef
         });
       }
+      // we use function below, to keep object in canvas area
       keepPosFixed(shape);
     });
 
